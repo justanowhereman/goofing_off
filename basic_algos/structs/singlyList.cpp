ab050d21df70be7f8b1b8cfec3a380c6aebca753
pandeyji <piyushp1203@jimsindia.org>
2025-01-28 20:51:25 +0530

    dataframes and series pandas

--- basic_algos/structs/singlyList.cpp
+++ basic_algos/structs/singlyList.cpp
@@ -1,1 +1,37 @@
-#
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+class Node
+{
+protected:
+    int data=0;
+    Node *next=nullptr;
+
+public:
+    void takein(int data); // insertion
+    void dele(int data);        // delete value
+    void del_at_pos(int a);  // del at pos
+    void display();     // display the list
+};
+Node* head=NULL;
+Node* current=nullptr;
+Node* tail=NULL;
+Node* temp = NULL;
+
+int main()
+{
+
+    return 0;
+}
+
+void Node::takein(int data){
+    if (head == NULL){
+        head = new Node();
+        head->data = data;
+    }
+    else{
+
+    }
+}
